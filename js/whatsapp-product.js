651ca726dd06076d6f363969052426841814c65c
shafee_azeem <shafeeghani@gmail.com>
2026-04-25 01:03:30 +0530

    whatsapp link fixed

--- js/whatsapp-product.js
+++ js/whatsapp-product.js
@@ -1,9 +1,10 @@
 function openWhatsApp() {
   const product = window.currentProduct;
+  const phone = "919901919978";
 
   const message = `Hi, I'm interested in ${product.title} (${product.pack}) for ${product.price}.`;
 
-  const url = `https://wa.me/91XXXXXXXXXX?text=${encodeURIComponent(message)}`;
+  const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
 
   window.open(url, "_blank");
 }
